421883eea74b62e1eafffe50d58f55c74279420b
Gaddiel Technologies <github@gaddieltech.com>
2016-03-30 10:52:39 +0530

    first commit

---
+++ src/gaddiel/malayalamrosary/Android.java
@@ -0,0 +1,11 @@
+package gaddiel.malayalamrosary;
+
+public class Android {
+	static String[] code = new String[]{
+		"Joyful Mysteries",
+		"Sorrowful Mysteries",
+		"Glorious Mysteries",
+		"Luminous Mysteries"
+		
+	};
+}
---
+++ src/gaddiel/malayalamrosary/Help.java
@@ -0,0 +1,22 @@
+package gaddiel.malayalamrosary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class Help extends Activity {
+	
+	private TextView mLink;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+		 super.onCreate(savedInstanceState);
+	       
+	        setContentView(R.layout.help);   
+	        mLink = (TextView) findViewById(R.id.textView2);
+	        mLink.setMovementMethod(LinkMovementMethod.getInstance());
+	      
+	        
+	   	}
+}
